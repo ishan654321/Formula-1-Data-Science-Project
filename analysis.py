fa6d01cf22ae9a5e0d7222595876e5f467ac2599
Ishan Vasavada <80118005+ishan654321@users.noreply.github.com>
2024-04-28 15:35:11 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load data for analysis
-pit_stop_df = pd.read_csv(r'D:\F1 track record csv\pit_stops_1.csv')
-drivers_df = pd.read_csv(r'D:\F1 track record csv\drivers_1.csv')
-races_df = pd.read_csv(r'D:\F1 track record csv\races.csv')
-lap_time_df=pd.read_csv(r'D:\F1 track record csv\lap_times_1.csv')
+pit_stop_df = pd.read_csv(r'pit_stops_1.csv')
+drivers_df = pd.read_csv(r'drivers_1.csv')
+races_df = pd.read_csv(r'races.csv')
+lap_time_df=pd.read_csv(r'lap_times_1.csv')
 
 def analyze_pitstop_duration():
     # Merge necessary tables
