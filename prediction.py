42256b95d0558c58d5d5890bebb03fb712d01c63
Ishan Vasavada <80118005+ishan654321@users.noreply.github.com>
2024-12-26 14:00:29 -0500

    Update prediction.py
    import added.

--- prediction.py
+++ prediction.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso  
+from sklearn.metrics import mean_squared_error
 #from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 # Load data for prediction
