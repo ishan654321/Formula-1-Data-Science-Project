7a4f75e484552b5b990ec6b7ab8afabe81fad40a
Ishan Vasavada <80118005+ishan654321@users.noreply.github.com>
2024-12-26 14:13:53 -0500

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -25,47 +25,6 @@ def predict_points(data):
     model = LinearRegression()
     model.fit(X_train, y_train)
     
-    # Predicting on the test set
-    #predictions = model.predict(X_test)
-
-    # Calculating mean squared error
-    #mse = mean_squared_error(y_test, predictions)
-
-    # Predicting 2024 points for all drivers
-    # predicted_2024_points = model.predict(X)
-
-    # # Adding predicted points to the dataframe
-    # data['predicted_2024_points'] = predicted_2024_points
-    # data['predicted_2024_points'] = data['predicted_2024_points'].round().clip(lower=0)
-    # data=data.sort_values(by=['predicted_2024_points'],ascending=False)
-
-    # # Displaying the results
-    # #st.write("Mean Squared Error:", mse)
-    # data_to_show = data[['driverRef','2021_points', '2022_points', '2023_points', 'predicted_2024_points']].to_html(index=False,classes='dataframe',border=0)
-    # styled_html = f'<style>table.dataframe{{background-color:black; color:white; padding:10px;}}</style>{data_to_show}'
-    # st.write(styled_html, unsafe_allow_html=True)
-    # plt.figure(figsize=(10, 4),facecolor='#463F41')
-    # ax = plt.axes()
-    # ax.set_facecolor("#463F41")
-    # bars=plt.bar(data['driverRef'], data['predicted_2024_points'], color='#E11433')
-    # plt.xlabel('Driver',color='white')
-    # plt.ylabel('Predicted 2024 Points',color='white')
-    # plt.title('Predicted 2024 Points for Each Driver',color='white')
-    # plt.xticks(rotation=90,color='white')
-    # plt.yticks(color='white')
-    # plt.tight_layout()
-
-    # for bar in bars:
-    #     yval = bar.get_height()
-    #     plt.text(bar.get_x(), yval, round(yval, 2), va='bottom',color='white')
-    # st.pyplot(plt)
-    # st.markdown('<img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2022-10/221010-Max-Verstappen-ew-1132a-276412.jpg" width="700" height="400"/>',unsafe_allow_html=True)
-
-    # st.write("""
-    # <div style="background-color: black; padding: 10px;">
-    #     <span style="color: white;">Note: Predicting sports results are not always accurate. I have used linear regression model to predict the data with the use of last 3 years of points data, driver skill rating, car rating and constructor strategy.</span>
-    # </div>
-    # """, unsafe_allow_html=True)
 def predict_points(data):
     st.header('Prediction: F1 Driver Points for 2024 Season')
 
@@ -82,13 +41,13 @@ def predict_points(data):
     X_test_scaled = scaler.transform(X_test)
     X_scaled = scaler.transform(X)
 
-    # Applying Ridge regression (L2 regularization)
-    model = Lasso(alpha=0.1)  # Adjust alpha for regularization strength (higher = stronger regularization)
+    # Applying Lasso regression (L1 regularization)
+    model = Lasso(alpha=0.1)  
     model.fit(X_train_scaled, y_train)
     
     # Predicting on the test set
     predictions = model.predict(X_test_scaled)
-    mse = mean_squared_error(y_test, predictions)  # Optionally calculate MSE
+    mse = mean_squared_error(y_test, predictions)  
     st.write(mse)
     # Predicting 2024 points for all drivers
     predicted_2024_points = model.predict(X_scaled)
@@ -125,7 +84,7 @@ def predict_points(data):
 
     st.write("""
     <div style="background-color: black; padding: 10px;">
-        <span style="color: white;">Note: Predicting sports results are not always accurate. I have used a Ridge regression model to predict the data with the use of the last 3 years of points data, driver skill rating, car rating, and constructor strategy.</span>
+        <span style="color: white;">Note: Predicting sports results are not always accurate. I have used a Lasso regression model(Regularization) to predict the data with the use of the last 3 years of points data, driver skill rating, car rating, and constructor strategy.</span>
     </div>
     """, unsafe_allow_html=True)
 #---------------------------------------------------------------------------------------
