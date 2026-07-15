00336f0d4cbe73988cc0ce50a1e7e440ec96c99b
Ishan Vasavada <80118005+ishan654321@users.noreply.github.com>
2026-07-14 23:58:54 -0400

    Add matplotlib for plotting in prediction.py

--- prediction.py
+++ prediction.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 from sklearn.impute import SimpleImputer
