68f9b3c1080cfaf66824731b9165080abf7ff694
Ishan Vasavada <80118005+ishan654321@users.noreply.github.com>
2026-07-14 23:57:47 -0400

    Refactor main.py to set environment variables

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
 import streamlit as st
 from analysis import analyze_pitstop_duration, lap_analysis
 from prediction import predict_points
